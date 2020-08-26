4efde9be1292289f94efe3f330d7e092d2181683
paulohfreire <paulo.freire53@gmail.com>
2020-08-25 23:05:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+            
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us17.api.mailchimp.com/3.0/lists/ff84e803ba";
+
+
+    const options = {
+        method: "POST",
+        auth: "paulof53:a1ea2f50e1f0dabb5cb361ffbc606375-us17"
+    }
+    const request = https.request(url, options, function (response) {
+         
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.send(__dirname + "/failure.html");
+
+        
+        }
+        
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/")
+})
+
+app.listen(3000, function () {
+    return
+});
+
